42f5e7a95a34d5cfe76060c55c24be6066c802e1
rotex <dantejake48@gmail.com>
2023-04-27 17:05:50 +0100

    add function doctstring

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,7 @@ from typing import Iterable, List, Tuple, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    return iterable object
+    """
     return [(i, len(i)) for i in lst]
