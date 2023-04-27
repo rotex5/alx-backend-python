2cc97b5271e6e5fec765abd57d1c226952c0e22a
rotex <dantejake48@gmail.com>
2023-04-27 16:43:48 +0100

    reduced function docstring and added execution mode to file

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 """
-Complex types - string and
-int/float to tuple
+Complex types - string andvint/float to tuple
 """
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple:
     """
-    -takes a string k and an int OR float v as arguments
-        and returns a tuple
-
-    -The first element of the tuple is the string k
-
-    - The second element is the square of the int/float v
-        and should be annotated as a float
+    takes a string k and an int OR float
+    v as arguments returns a tuple
     """
     sq_v: float = v ** 2
     return (k, sq_v)
