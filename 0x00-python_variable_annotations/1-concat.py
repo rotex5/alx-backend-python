f56dcc206f5e2bfe4cb285a2972222dc6cd5fe44
rotex <dantejake48@gmail.com>
2023-04-27 16:03:10 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    takes a string str1 and a string str2
+    as arguments and returns a concatenated string
+    """
+    return "{}{}".format(str1, str2)
