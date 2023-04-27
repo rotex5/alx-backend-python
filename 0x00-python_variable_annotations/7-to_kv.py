95ff8585956d87364d61d63a345c3ae3e222be60
rotex <dantejake48@gmail.com>
2023-04-27 16:49:09 +0100

    added annotation for return tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,10 +5,13 @@ Complex types - string andvint/float to tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    takes a string k and an int OR float
-    v as arguments returns a tuple
+    -takes a string k and an int OR float v as arguments
+        and returns a tuple
+    -The first element of the tuple is the string k
+    - The second element is the square of the int/float v
+        and should be annotated as a float
     """
     sq_v: float = v ** 2
     return (k, sq_v)
