ebe336f30adc8e2ea366d225ef41fa83ce345398
rotex <dantejake48@gmail.com>
2023-04-27 15:59:23 +0100

    basic function and argument annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    takes a float a and a float b as
+    arguments and returns their sum as a float
+    """
+    sum: float = a + b
+    return sum
