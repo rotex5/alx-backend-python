9f878a88e6ac8ee5b507a40323f5940e5889bae9
rotex <dantejake48@gmail.com>
2023-04-27 17:22:49 +0100

    used an instance of Typevar class

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,10 +4,12 @@ More involved type annotations
 """
 from typing import Any, Mapping, TypeVar, Union
 
+T = TypeVar('T')
+
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[TypeVar, None] = None
-                     ) -> Union[Any, TypeVar]:
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
     """
     return duk typed dictioay key or default
     """
