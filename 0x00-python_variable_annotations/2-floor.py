f4760bccaae800eeee8f7061c928050309fbc126
rotex <dantejake48@gmail.com>
2023-04-27 16:08:54 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+from math import floor as fl
+
+
+def floor(n: float) -> int:
+    """
+    takes a float n as argument
+    and returns the floor of the float
+    """
+    return fl(n)
