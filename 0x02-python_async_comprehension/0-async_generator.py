94f11fb580767dbd94e6e0bc3443856d87e976b5
rotex <dantejake48@gmail.com>
2023-05-03 17:16:22 +0100

    ran sleep before yield generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,5 +13,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     between 0 and 10
     """
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
