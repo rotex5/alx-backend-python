fad60f14fe3a0f8232ff79e09f10ce8c5c5caa17
rotex <dantejake48@gmail.com>
2023-05-21 08:59:14 +0100

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,11 @@
 from client import GithubOrgClient
 from parameterized import parameterized
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import (
+    MagicMock,
+    PropertyMock,
+    patch,
+)
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -12,17 +16,32 @@ class TestGithubOrgClient(unittest.TestCase):
     Testing Github Org Client
     """
     @parameterized.expand([
-        ('google'),
-        ('abc')
+        ("google"),
+        ("abc")
     ])
-    @patch('client.get_json')
+    @patch("client.get_json")
     def test_org(self,
                  org: str,
                  mock: MagicMock) -> None:
         """
         Testing that GithubOrgClient.org returns expected value
         """
-        test_class = GithubOrgClient(org)
-        test_class.org()
+        test_client = GithubOrgClient(org)
+        test_client.org()
         mock.assert_called_once_with(
                 "https://api.github.com/orgs/{}".format(org))
+
+    def test_public_repos_url(self) -> None:
+        """
+        Testing that _public_repos_url returns expected value
+        based on the mocked payload
+        """
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock
+                   ) as mock:
+            payload = {
+                    "repos_url": "https://api.github.com/users/abc"}
+            mock.return_value = payload
+            test_client = GithubOrgClient("abc")
+            result = test_client._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
