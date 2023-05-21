1de4f803ee4fbda573c073a0c1f4e872d77e9704
rotex <dantejake48@gmail.com>
2023-05-21 07:57:32 +0100

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -34,3 +34,19 @@ class TestAccessNestedMap(unittest.TestCase):
         Testing method for access_nested_map
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: str):
+        """
+        Testing KeyError raised based on the respective inputs
+        """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual("'{}'".format(expected), str(e.exception))
